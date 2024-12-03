d44e88bb0bb1b3e5559496fa0da90ba1277d1888
BINHTRAN\binh0 <22115053122103@sv.ute.udn.vn>
2024-12-03 14:35:32 +0700

    Login

---
+++ cuoiki_LTWNC/cuoiki_LTWNC/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using cuoiki_LTWNC.Models;
+using System.Data.SqlClient;
+namespace cuoiki_LTWNC.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Finish()
+        {
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "Data Source=BINHTRAN\\BINHVAN;Initial Catalog=Login;Integrated Security=True;Encrypt=False";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Login where username='" + acc.MSV + "' and password='" + acc.Password + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Finish");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+        }
+
+
+
+        //public ActionResult Login()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Login(Account acc)
+        //{
+        //    var appDbContext = new AppDbContext();
+        //}
+    }
+}
---
+++ cuoiki_LTWNC/cuoiki_LTWNC/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+
+namespace cuoiki_LTWNC.Models
+{
+    public class Account
+    {
+        //[Required(ErrorMessage = "MSV cannot be blank")]
+        public string MSV { get; set; }
+        //[Required(ErrorMessage = "Password cannot be blank")]
+        public string Password { get; set; }
+    }
+}
